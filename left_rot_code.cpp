7d9c2ab5a51c40318d151395f3621b2ea8e19af2
pranjal-srivastava <30296549+pranjal-srivastava@users.noreply.github.com>
2019-06-12 22:25:24 +0530

    self_function_rot_array_main

--- left_rot_code.cpp
+++ left_rot_code.cpp
@@ -42,76 +42,22 @@ int main()
    // a= input vector n=vector size  d= rotate amount
    // vector<int> result = rotLeft(a, d);
    /*
-   ofstream fout(getenv("OUTPUT_PATH"));
-
-    string nd_temp;
-    getline(cin, nd_temp);
-
-    vector<string> nd = split_string(nd_temp);
-
-    int n = stoi(nd[0]);
-
-    int d = stoi(nd[1]);
-
-    string a_temp_temp;
-    getline(cin, a_temp_temp);
-
-    vector<string> a_temp = split_string(a_temp_temp);
-
-    vector<int> a(n);
-
-    for (int i = 0; i < n; i++) {
-        int a_item = stoi(a_temp[i]);
-
-        a[i] = a_item;
+   int n;
+    int d;
+    cin>>n>>d;
+    vector<int>a(n);
+    int arr_i;
+    for(int i=0;i<n;i++)
+    {
+      cin>>arr_i;
+      a.push_back(arr_i);
     }
-
+    
     vector<int> result = rotLeft(a, d);
-
-    for (int i = 0; i < result.size(); i++) {
-        fout << result[i];
-
-        if (i != result.size() - 1) {
-            fout << " ";
-        }
-    }
-
-    fout << "\n";
-
-    fout.close();
-
+    
+    //display the vector
+    
     return 0;
-}
-
-vector<string> split_string(string input_string) {
-    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
-        return x == y and x == ' ';
-    });
-
-    input_string.erase(new_end, input_string.end());
-
-    while (input_string[input_string.length() - 1] == ' ') {
-        input_string.pop_back();
-    }
-
-    vector<string> splits;
-    char delimiter = ' ';
-
-    size_t i = 0;
-    size_t pos = input_string.find(delimiter);
-
-    while (pos != string::npos) {
-        splits.push_back(input_string.substr(i, pos - i));
-
-        i = pos + 1;
-        pos = input_string.find(delimiter, i);
-    }
-
-    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
-
-    return splits;
-}
-
    */
    
 }
